362329cafefc10662f4a6b728215055b2f3a26fe
qpeng <qpeng@macalester.edu>
2014-02-06 10:44:24 -0600

    Game of Life (Qinghao Peng)

--- src/edu/macalester/comp124/life/HighLife.java
+++ src/edu/macalester/comp124/life/HighLife.java
@@ -5,10 +5,10 @@ package edu.macalester.comp124.life;
  *
  * @author Michael Ekstrand <ekstrand@cs.umn.edu>
  */
-public class Conway implements RuleSet {
+public class HighLife implements RuleSet {
 
     public String getName() {
-        return "Conway's Rules";
+        return "HighLife";
     }
 
     /**
@@ -24,7 +24,7 @@ public class Conway implements RuleSet {
             else {return isAlive;}
         }
         else{
-            if (neighborCount == 3) {return true;}
+            if ((neighborCount == 3)||(neighborCount == 6)) {return true;}
             else {return isAlive;}
         }
     }
